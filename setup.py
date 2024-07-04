5649147f7433b2ec739864f8657bb46d2808b04a
Christopher Kuenneth <christopher.kuenneth@gmail.com>
2024-07-04 15:37:59 +0200

    Revert building without Windows the python dll

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class BuildRDKit(build_ext_orig):
             ]
         )
         
-        without_python_lib = "boost:without_python_lib=True"
+        without_python_lib = "boost:without_python_lib=False"
         boost_version_string = f"boost/{boost_version}@chris/mod_boost"
         without_stacktrace = "False"
     
